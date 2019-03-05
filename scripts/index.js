0fce9ac7494a8b831e71505f2fff2e9cf4f12a23
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-05 07:25:01 -0500

    completed 1337 speak

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+logger.init()
+
 // function hello(name='world') {
 //     return `Hello, ${name}!`
 // }
@@ -10,6 +12,7 @@
 
 // console.log(madlib("cat","running from vacuums"))
 
+
 function tipAmount(bill, service){
     let tipResult = 0
     if(service === 'good'){
@@ -20,7 +23,7 @@ function tipAmount(bill, service){
         return (bill * .1);
     }
 }
-console.log(tipAmount(100, 'good'))
+// console.log(tipAmount(100, 'good'))
 
 function totalAmount(bill, service){
     switch(service){
@@ -33,7 +36,7 @@ function totalAmount(bill, service){
     }
 }
 
-console.log(totalAmount(100,'good'))
+// console.log(totalAmount(100,'good'))
 
 function splitAmount(bill, service, split){
     switch(service){
@@ -45,4 +48,108 @@ function splitAmount(bill, service, split){
             return (bill + (bill * .1)) / split;
     }
 }
-console.log(splitAmount(100,'good',3))
+// console.log(splitAmount(100,'good',2))
+
+function printNumbers(a, b){
+    for (let i = a; i <= b; i++) {
+        console.log(i)
+    }
+}
+// printNumbers(1,10)
+
+function printSquare(aNum){
+    for (let i = 0; i < aNum; i++){
+        let square = '*'
+        console.log(square.repeat(aNum))
+    }
+}
+// printSquare(10)
+
+function printBox(w,h){
+    star = "*"
+    space = " "
+    for (let i = 1; i <= h; i++) {
+        if (i === 1){
+            console.log(star.repeat(w))
+        } else if (i > 1 && i != h){
+            console.log(star + space.repeat(w - 2) + star)
+        } else{
+            console.log(star.repeat(w))
+        }
+    }
+}
+// printBox(6,10)
+
+
+function printBanner(text) {
+    star = "*"
+    console.log(star.repeat(text.length + 4))
+    console.log(`* ${text} *`)
+    console.log(star.repeat(text.length + 4))
+}
+// printBanner("I AM THE ONE TRUE HEIR")
+
+function factorFinder(aNum){
+    results = []
+    for (let i = 1; i <= aNum; i++){
+        if (aNum % i === 0) {
+            results.push(i)
+        }
+    }
+    return results
+}
+
+// console.log(factorFinder(100))
+
+function caeserCipher(phrase, offset){
+    const cipher = "abcdefghijklmnopqrstuvwyxz ";
+    let result = '';
+    let index = 0;
+    for (let i = 0; i < phrase.length; i++){
+        index = cipher.indexOf(phrase[i])
+        index += offset
+        if (index > cipher.length){
+            index += -cipher.length
+        }
+        result += cipher[index]
+    }
+    return result
+}
+// console.log(caeserCipher("hellooo vacuum kitties", 2))
+
+function caeserDecipher(phrase, offset){
+    const cipher = "abcdefghijklmnopqrstuvwyxz ";
+    let result = '';
+    let index = 0;
+    for (let i = 0; i < phrase.length; i++){
+        index = cipher.indexOf(phrase[i])
+        index -= offset
+        if (index < 0){
+            index += cipher.length
+        }
+        result += cipher[index]
+    }
+    return result
+}
+
+// console.log(caeserDecipher("jgnnqqqbycewwobmkvvkgu", 2))
+
+function leetSpeak(phrase){
+    letter = ['A','E','G','I','O','S','T'];
+    leet = ['4','3','6','1','0','5','7'];
+    results = '';
+    const upperPhrase = phrase.toUpperCase();
+    for (let i = 0; i < upperPhrase.length; i++){
+        myLetter = upperPhrase[i]
+            if (letter.includes(myLetter)) {
+                results += leet[letter.indexOf(myLetter)]
+            } else{
+                results += myLetter
+            }
+    }
+    return results
+}
+
+
+// console.log(leetSpeak("THIS IS YOUR HEIR SPEAKING!"))
+
